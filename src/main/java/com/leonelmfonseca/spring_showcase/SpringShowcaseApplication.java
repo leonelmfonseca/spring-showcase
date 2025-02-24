14e0d4a33cda915b5c58769cae7c007e7be508b0
leonelmfonseca <leonelfonseca@gmail.com>
2025-02-24 17:20:04 +0000

    chore: commit default files

---
+++ src/main/java/com/leonelmfonseca/spring_showcase/SpringShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.leonelmfonseca.spring_showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonelmfonseca/spring_showcase/SpringShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonelmfonseca.spring_showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
